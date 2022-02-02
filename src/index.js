90f64488071e79e646a86e6355d76746379dda1b
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2022-02-02 18:31:59 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const string = String(n); // приобразование в строку
+    const array = Array.from(string); //создание массива из строки
+    let newString = '';
+    let i = array.length - 1;
+    let j = 0;
+    while ((i >= 0) && (j < 3)) {
+        newString += array[i];
+        i--;
+        j++;
+    }
+    return Number(newString)
 }
